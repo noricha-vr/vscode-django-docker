481814ff9685a99afc400cb1db25647b2b2b2e92
noricha-vr <noricha.vr@gmail.com>
2023-05-04 07:23:48 +0900

    success stop on template

--- app/app/views.py
+++ app/app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def index(request):
     # return HttpResponse("Hello")
-    return render(request, "app/index.html")
+    return render(request, "app/index.html", {"a": 1})
